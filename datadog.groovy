e26ba1c0ee22ebfa24633e083d57fbd7a77dce85
Amit Sethi <amisethi@ciena.com>
2022-10-25 13:45:53 +0530

    testing datadog

--- datadog.groovy
+++ datadog.groovy
@@ -1,5 +1,11 @@
-void dataDogIntegration() {
-	print "hello from datadog"
+void dataDogIntegration(def targetWorkspace) {
+	def targetPath =  "${targetWorkspace}/src/main/resources/"
+	print "----------------Datadog Integration Started----------------"
+	sh "rm ${targetPath}/log4j2.xml"
+	print "Removed log4j2 from ${targetPath}"
+	sh "cp resources/log4j2.xml ${targetPath}"
+	print "Copied datadog log4j2 to ${targetPath}"
+	
 }
 
 return this
