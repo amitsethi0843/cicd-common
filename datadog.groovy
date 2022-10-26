1d98772570e840b1f6d10d5911cc0d9374c2e455
Amit Sethi <amisethi@ciena.com>
2022-10-26 18:05:30 +0530

    path fix

--- datadog.groovy
+++ datadog.groovy
@@ -1,7 +1,7 @@
 void dataDogIntegration(def targetWorkspace) {
 	def targetPath =  "${targetWorkspace}/src/main/resources/"
 	print "----------------Datadog Integration Started----------------"
-	sh "rm ${targetPath}/log4j2.xml"
+	sh "rm ${targetPath}log4j2.xml"
 	print "Removed log4j2 from ${targetPath}"
 	sh "cp resources/log4j2.xml ${targetPath}"
 	print "Copied datadog log4j2 to ${targetPath}"
@@ -9,3 +9,7 @@ void dataDogIntegration(def targetWorkspace) {
 }
 
 return this
+
+
+
+
