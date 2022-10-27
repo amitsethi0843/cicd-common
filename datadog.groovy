a341b342a469f3e1102842e04dd53ac4e843e871
Amit Sethi <amisethi@ciena.com>
2022-10-27 10:33:57 +0530

    testing sudo

--- datadog.groovy
+++ datadog.groovy
@@ -1,9 +1,9 @@
 void dataDogIntegration(def targetWorkspace) {
 	def targetPath =  "${targetWorkspace}/src/main/resources/"
 	print "----------------Datadog Integration Started----------------"
-	sh "rm ${targetPath}log4j2.xml"
+	sh "sudo rm ${targetPath}log4j2.xml"
 	print "Removed log4j2 from ${targetPath}"
-	sh "cp resources/log4j2.xml ${targetPath}"
+	sh "sudo cp resources/log4j2.xml ${targetPath}"
 	print "Copied datadog log4j2 to ${targetPath}"
 	
 }
