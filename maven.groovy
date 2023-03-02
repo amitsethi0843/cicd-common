11795754c72176eb5920e469f8971c3828ac3bd9
Amit Sethi <amisethi@ciena.com>
2023-03-02 10:09:58 +0530

    updated fix

--- maven.groovy
+++ maven.groovy
@@ -11,7 +11,7 @@ void deploy (def deployParams){
 		throw new Exception(" ${it} key is missing from the deployment parameters")
 		}
 
-	sh "mvn clean deploy -DmuleDeploy -DskipTests -Dcloudhub.username=${deployParams.chUser} -P=${deployParams.env} -Dcloudhub.workers=${deployParams.totalWorkers} -Dcloudhub.worker_type=${deployParams.workerSize} -Dcloudhub.password=${deployParams.chPassword} -Dcloudhub.properties.client_id=${deployParams.chClientID} -Dcloudhub.properties.client_secret=${deployParams.chClientSecret} -Dcloudhub.properties.mule_key=${deployParams.key} -Dcloudhub.properties.mule_securekey=${deployParams.secureKey}  -Dcloudhub.business_group='${deployParams.businessGroup}' -Dcloudhub.properties.ddApiKey=${deployParams.ddApiKey}"
+	sh "mvn clean deploy -DmuleDeploy -DskipTests -Dcloudhub.username=${deployParams.chUser} -P=${deployParams.env} -Dcloudhub.workers=${deployParams.totalWorkers} -Dcloudhub.worker_type=${deployParams.workerSize} -Dcloudhub.password=${deployParams.chPassword} -Dcloudhub.properties.client_id=${deployParams.chClientID} -Dcloudhub.properties.client_secret=${deployParams.chClientSecret} -Dcloudhub.properties.mule_key=${deployParams.key} -Dcloudhub.properties.mule_securekey=${deployParams.secureKey}  -Dcloudhub.business_group=${deployParams.businessGroup} -Dcloudhub.properties.ddApiKey=${deployParams.ddApiKey}"
 }
 
 return this
